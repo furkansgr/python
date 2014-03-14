7fc6c5496ad9673048ea4cfdc736c30651f9228c
kenansubasi <kenansubasiceng@gmail.com>
2014-03-15 00:28:00 +0200

    4 basamaklı orjinalsayıları bulan program.

---
+++ orjinal_sayi.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+
+for sayi in range(1000,10000):
+    sayi = str(sayi)
+    a = int(sayi[0]+sayi[1])
+    b = int(sayi[2]+sayi[3])
+    sayi = int(sayi)
+    if ((a*a)+(b*b)) == sayi:
+        print sayi
+
+
+
+
+
