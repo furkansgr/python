750823b31ca3518132ef065a96c763275e0f0497
KENAN SUBAŞI <kenansubasiceng@gmail.com>
2014-03-19 10:12:50 +0200

    Update karekök2.py

--- karekök2.py
+++ karekök2.py
@@ -1,15 +1,12 @@
 #-*- coding:utf-8 -*-
-#Karekök 2 sayısının bulunuşu. 
+#Karekök 2 sayısın bulunuşu. 
 from __future__ import division
 
 Z1 = 2+(1/3)
 Z2 = 0
-n = 0
 
-while n < 20:
-    Z2 = 1+(1/(2+1/Z1))
-    Z1 = Z2
-    n += 1
+Z2 = 1+(1/(2+1/Z1))
+Z1 = Z2
 
 print Z2
  
